305db695091ae3a8d268c9264385ef1ba748b4ea
lmTinelli <luizmarcelotinelli@gmail.com>
2026-07-03 20:42:34 -0300

    Adicionando aulas de dados - pandas, numpy e seaborn

---
+++ app.py
@@ -0,0 +1,12 @@
+# Comando para exibir informações na tela
+print ("Olá boa noite!")
+
+idade = 30
+idade_dois = "30"
+idade_tres = 30.5
+
+#Retorna o tipo da variável
+print (type(idade))
+print(type(idade_dois))
+print(type(idade_tres))
+
